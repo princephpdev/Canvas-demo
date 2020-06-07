d551b740a060563d8c80555341531057d4789fee
Prince K <princephpdev@gmail.com>
2020-06-07 22:57:40 +0530

    Added Frontend with Canvas Studio

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -67,6 +67,7 @@
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
+                <a href="{{ route('studio') }}"> Studio</a>
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
@@ -81,18 +82,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    Laravel with Canvas Studio
                 </div>
             </div>
         </div>
